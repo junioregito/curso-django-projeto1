a2531b80ae0bebfa1d441c35f06886764479a979
Junior Egito <junioregito@gmail.com>
2022-03-03 15:58:25 -0400

    128

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^f)^04k@_d=i93k*)h3v+5tz+8^^6vv$#7!r$y)6rtww#=ta79'  # noqa: E501
+SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
 ALLOWED_HOSTS: list[str] = []
 
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # A gente tem apps também
     'recipes',
+    'authors',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +137,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'message-debug',
+    constants.ERROR: 'message-error',
+    constants.INFO: 'message-info',
+    constants.SUCCESS: 'message-success',
+    constants.WARNING: 'message-warning',
+}
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,23 @@
+#from utils.recipes.factory import make_recipe
+#from django.core.paginator import Paginator
+import os
+
+
+from django.db.models import Q
+from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
-from utils.recipes.factory import make_recipe
+from utils.pagination import make_pagination
 
 # ou desse jeito  from .models import Recipe
 from recipes.models import Recipe
 
+# ou somente os pacotesfrom django.contrib.messages import success, error, debug
+# nessa caso serira
+
+
 # Create your views here.
+
+PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
 
 def home(request):
@@ -12,8 +25,13 @@ def home(request):
         is_published=True
     ).order_by('-id')
 
+    page_obj, pagination_range = make_pagination(request, receitas, PER_PAGE)
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': receitas,
+        #        'recipes': receitas,
+        'recipes': page_obj,
+        'pagination_range': pagination_range
+
         #        'recipes': [make_recipe() for _ in range(10)],
     })
 
@@ -26,8 +44,11 @@ def category(request, categoria_id):
         ).order_by('-id')
     )
 
+    page_obj, pagination_range = make_pagination(request, receitas, PER_PAGE)
+
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': receitas,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
         'title': f'{receitas[0].category.name} - Category | '
     })
 
@@ -38,4 +59,29 @@ def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': receita,
         'is_detail_page': True,
+    })
+
+
+def search(request):
+    search_term = request.GET.get('q', '').strip()
+
+    if not search_term:
+        raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
+        'additional_url_query': f'&q={search_term}',
     })
